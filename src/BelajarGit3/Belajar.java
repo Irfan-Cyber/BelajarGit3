020cc52f4f977510c667ce769d78997320d54f08
Irfan-Cyber <55918863+Irfan-Cyber@users.noreply.github.com>
2019-09-29 10:44:04 +0700

    Mengubah tulisan Hello World

--- src/BelajarGit3/Belajar.java
+++ src/BelajarGit3/Belajar.java
@@ -2,6 +2,6 @@ package BelajarGit3;
 
 public class Belajar {
     public static void main(String[] args){
-        System.out.println("Hello world");
+        System.out.println("Hello world, saya udah ubah tulisannya");
     }
 }
