665768fe6b447307faa0d78f23158902083a1942
Irfan-Cyber <55918863+Irfan-Cyber@users.noreply.github.com>
2019-09-29 10:57:12 +0700

    Menambahkan file baru kucing.java

---
+++ src/BelajarGit3/Kucing.java
@@ -0,0 +1,12 @@
+package BelajarGit3;
+
+public class Kucing {
+
+    private static void bicara(){
+        System.out.println("meong");
+    }
+
+    public static void main(String[] args){
+        bicara();
+    }
+}
