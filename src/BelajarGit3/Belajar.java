a8efaf8be352d841cc248f8a00e7078935d0418f
Irfan-Cyber <55918863+Irfan-Cyber@users.noreply.github.com>
2019-09-29 11:20:18 +0700

    Menambah komponen data pada class

--- src/BelajarGit3/Belajar.java
+++ src/BelajarGit3/Belajar.java
@@ -1,15 +1,17 @@
 package BelajarGit3;
-
 public class Belajar {
     public static void main(String[] args){
         int umur;
         String nama;
-        String cita;
+        String jabatan;
         System.out.println("Hello world, saya udah ubah tulisannya");
 
         umur = 17;
         nama = "Si ganteng";
-        cita = "Penyihir";
-        System.out.println("Nama: "+nama+"\n"+"Umur: "+umur+"\n"+"cita-cita: "+cita);
+        jabatan = "Penyihir";
+
+        Manusia bejo = new Manusia(umur,nama,jabatan);
+
+        System.out.println("Nama: "+bejo.getNama()+"\n"+"Umur: "+bejo.getUmur()+"\n"+"cita-cita: "+bejo.getJabatan());
     }
 }
