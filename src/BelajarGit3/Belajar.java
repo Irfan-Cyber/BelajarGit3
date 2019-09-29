612ead8809c3b3cc83edfabc0906537d40a75ed0
Irfan-Cyber <55918863+Irfan-Cyber@users.noreply.github.com>
2019-09-29 10:56:39 +0700

    Menambahkan variabel untuk menampilkan info

--- src/BelajarGit3/Belajar.java
+++ src/BelajarGit3/Belajar.java
@@ -2,6 +2,14 @@ package BelajarGit3;
 
 public class Belajar {
     public static void main(String[] args){
+        int umur;
+        String nama;
+        String cita;
         System.out.println("Hello world, saya udah ubah tulisannya");
+
+        umur = 17;
+        nama = "Si ganteng";
+        cita = "Penyihir";
+        System.out.println("Nama: "+nama+"\n"+"Umur: "+umur+"\n"+"cita-cita: "+cita);
     }
 }
